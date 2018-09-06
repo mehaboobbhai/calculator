7119420e98c175bf47e46ca1d3c6967c66b9503d
mehaboob <mehaboobpasha18@gmail.com>
2018-09-06 15:27:29 +0530

    changes made in division

--- src/main/java/com/teamsankya/calculator/Division.java
+++ src/main/java/com/teamsankya/calculator/Division.java
@@ -3,12 +3,15 @@ package com.teamsankya.calculator;
 public class Division {
 	public static int div(int a, int b) {
 
+		
+		System.out.println("changes happens");
 		return a / b;
 
 	}
 
 	public static double div(double a, double b) {
 
+		System.out.println("changes happens");
 		return a / b;
 
 	}
